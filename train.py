2d26a6db44ec4cf3e6f3f09aa716f50ed864bb9a
Mingjie <xuming1@ecs.ac.nz>
2019-10-14 21:12:09 +1300

    with training model

--- train.py
+++ train.py
@@ -10,9 +10,13 @@ However, you must make sure:
 
 ©2019 Created by Yiming Peng and Bing Xue
 """
+from keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras import backend as K
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
 
 import numpy as np
 import tensorflow as tf
@@ -23,7 +27,10 @@ SEED = 309
 np.random.seed(SEED)
 random.seed(SEED)
 tf.set_random_seed(SEED)
-
+if K.image_data_format == 'channels_first':
+    input_shape = (3, 300, 300)
+else:
+    input_shape = (300, 300, 3)
 
 def construct_model():
     """
@@ -38,15 +45,35 @@ def construct_model():
     :return: model: the initial CNN model
     """
     model = Sequential()
-    model.add(Dense(units=64, activation='relu', input_dim=100))
-    model.add(Dense(units=10, activation='softmax'))
+    model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+    model.add(Dense(64))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+
+    # model.add(Dense(units=64, activation='relu', input_dim=100))
+    # model.add(Dense(units=10, activation='softmax'))
+    model.add(Dense(3, activation='softmax'))
     model.compile(loss='categorical_crossentropy',
               optimizer='sgd',
               metrics=['accuracy'])
     return model
 
 
-def train_model(model):
+def train_model(model, train_x, train_y):
     """
     Train the CNN model
     ***
@@ -56,7 +83,11 @@ def train_model(model):
     :return:model:   the trained CNN model
     """
     # Add your code here
-    # model.fit_generator()
+    print(train_y.image_shape)
+    model.fit_generator(train_x,
+                        steps_per_epoch=2000,
+                        epochs=15,
+                        validation_data=train_y)
     return model
 
 
@@ -70,11 +101,30 @@ def save_model(model):
     #   Please remove the comment to enable model save.
     #   However, it will overwrite the baseline model we provided.
     # ***
-    # model.save("model/model.h5")
+    model.save("./model/model.h5")
     print("Model Saved Successfully.")
 
 
+def load_data(path):
+    datagen = ImageDataGenerator(
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        rescale=1. / 255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest',
+        validation_split=0.2)
+    train = datagen.flow_from_directory(path, target_size=(300, 300), classes=['cherry', 'strawberry', 'tomato'],
+                                          batch_size=16, class_mode='categorical', subset='training')
+
+    test = datagen.flow_from_directory(path, target_size=(300, 300), classes=['cherry', 'strawberry', 'tomato'],
+                                       batch_size=16, class_mode='categorical', subset='validation')
+    return train, test
+
 if __name__ == '__main__':
+    train_x, train_y = load_data("./data")
     model = construct_model()
-    model = train_model(model)
-    save_model(model)
+    model = train_model(model, train_x, train_y)
+    # save_model(model)
